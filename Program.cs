cd50ca1a9a9c72f6eb0310f6f9d95a106f443abc
Дмитрий Барышев <dmitriy_baryshev@list.ru>
2022-02-07 03:42:16 +0700

    Updated menu

--- Program.cs
+++ Program.cs
@@ -5,45 +5,60 @@ using System.IO;
 
 namespace ConsoleApplication
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[]args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("\t\t\t Enter the keyword to select\n");
-            string usr_inp, s; // usr_inp - строка для пользовательского ввода, s - строка для считывания текстового файла
-            try
+            do
             {
-#pragma warning disable CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
-                usr_inp = Console.ReadLine(); 
-#pragma warning restore CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-            try
-            {
-                StreamReader f = new StreamReader("input.txt");
-                s = f.ReadToEnd();
-                f.Close();
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Check correct name of the file");
-                return;
-            }
-            if (s.Length > 0) // Если строка не пустая
-            {
-                string[] Sentences = s.Split(new char[] { '.','!','?' }); // Разбиение строки на подстроки в массив строк Sentences по литералам '.', '!', '?'
-                for (int i = 0; i < Sentences.Length; i++)
-#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
-                    if (Sentences[i].Contains(usr_inp)) // Проверка каждого предложения на содержание usr_inp
-#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
-                        Console.WriteLine(Sentences[i]);
-            }
-            Console.WriteLine();
+                Console.WriteLine("\t\t\t 1. Enter the keyword to select\n");
+                Console.WriteLine("\t\t\t 2. Exit\n");
+                string usr_inp, s; // usr_inp - строка для пользовательского ввода, s - строка для считывания текстового файла
+                try
+                {
+                    usr_inp = Console.ReadLine();
+                    switch (int.Parse(usr_inp))
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("\t\t\t Please enter the keyword\n");
+                                usr_inp = Console.ReadLine();
+                                try
+                                {
+                                    StreamReader f = new StreamReader("input.txt");
+                                    s = f.ReadToEnd();
+                                    f.Close();
+                                }
+                                catch (FileNotFoundException e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                    Console.WriteLine("Check correct name of the file");
+                                    return;
+                                }
+                                if (s.Length > 0) // Если строка не пустая
+                                {
+                                    string[] Sentences = s.Split(new char[] { '.', '!', '?' }); // Разбиение строки на подстроки в массив строк Sentences по литералам '.', '!', '?'
+                                    for (int i = 0; i < Sentences.Length; i++)
+                                    {
+                                        if (Sentences[i].Contains(usr_inp))
+                                        {
+                                            Console.WriteLine(Sentences[i]);
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                        case 2: return;
+
+                        default: return;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            } while (true);
         }
     }
 }
